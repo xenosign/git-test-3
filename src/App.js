a23892467a469912d620a815f9401e0c571e6d41
ZZAME <92978598+DEVZZAME@users.noreply.github.com>
2022-10-19 15:19:50 +0900

    회원가입 페이지 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Index from './pages/Index';
 import Item from './pages/Item';
 import Join from './pages/Join';
 import Login from './pages/Login';
+import Signup1 from './pages/Signup1';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/item" element={<Item />} />
       <Route path="/join" element={<Join />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup1" element={<Signup1 />} />
     </Routes>
   );
 }
