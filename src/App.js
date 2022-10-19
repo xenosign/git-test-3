17373542f52f1f5695b2c5ab596a63257b61bf98
MO <48742497+msh9900@users.noreply.github.com>
2022-10-19 15:33:25 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,9 @@ import Index from './pages/Index';
 import Item from './pages/Item';
 import Join from './pages/Join';
 import Login from './pages/Login';
+import Signup2 from './pages/Signup2';
 import Signup1 from './pages/Signup1';
-
-import { Route, Routes } from "react-router-dom";
-import Index from "./pages/Index";
-import Item from "./pages/Item";
-import Join from "./pages/Join";
-import Login from "./pages/Login";
-
-import Signup2 from "./pages/Signup2";
-import Signup3 from "./pages/Signup3";
-
+import Signup3 from './pages/Signup3';
 
 function App() {
   return (
@@ -22,9 +14,7 @@ function App() {
       <Route path="/item" element={<Item />} />
       <Route path="/join" element={<Join />} />
       <Route path="/login" element={<Login />} />
-
       <Route path="/signup2" element={<Signup2 />} />
-
       <Route path="/signup1" element={<Signup1 />} />
       <Route path="/signup3" element={<Signup3 />} />
     </Routes>
